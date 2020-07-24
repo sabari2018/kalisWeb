ab39fb98c84388b3534249cda95cfcc50dccafd2
Sadou <sadouship@gmail.com>
2020-07-24 16:50:14 +0200

    intergration du template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontendWeb';
+  appName = 'M@ri Pay';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './squelette/nav/nav.component';
+import { HeaderComponent } from './squelette/header/header.component';
+import { FooterComponent } from './squelette/footer/footer.component';
+import { EtapeComponent } from './accueil/etape/etape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HeaderComponent,
+    FooterComponent,
+    EtapeComponent
   ],
   imports: [
     BrowserModule
